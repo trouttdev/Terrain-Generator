057584713a234f07235c4ae76c9a14489984aa20
Michael Troutt <mltroutt@gmail.com>
2012-04-24 13:02:00 -0500

    Added commenting

--- game.py
+++ game.py
@@ -1,85 +1,105 @@
 import sys, pygame, random
 pygame.init()
 
+#Define game area
 size = width, height = 500,500
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
-
-terrainSize = 2
-
 screen.fill((0,0,0))
 
-terrain = {'DEEP_OCEAN',
-           'SHALLOW_OCEAN',
-           'SHALLOW_WATER',
-           'SAND',
-           'GRASS',
-           'HILL',
-           'LOW_MOUNTAIN',
-           'HIGH_MOUNTAIN'}
+#Set the terrain pixel size
+terrainSize = 2
 
-terrain_val = {0:(0,0,128),
-               1:(0,0,255),
-               2:(163,163,255),
-               3:(255,229,148),
-               4:(20,210,24),
-               5:(14,140,17),
-               6:(162,162,162),
-               7:(220,220,220)}
+#terrain = {'DEEP_OCEAN',
+#           'SHALLOW_OCEAN',
+#           'SHALLOW_WATER',
+#           'SAND',
+#           'GRASS',
+#           'HILL',
+#           'LOW_MOUNTAIN',
+#           'HIGH_MOUNTAIN'}
+
+terrain_val = {0:(0,0,128),     #DEEP_OCEAN
+               1:(0,0,255),     #SHALLOW_OCEAN
+               2:(163,163,255), #SHALLOW_WATER
+               3:(255,229,148), #SAND
+               4:(20,210,24),   #GRASS
+               5:(14,140,17),   #HILL
+               6:(162,162,162), #LOW_MOUNTAIN
+               7:(220,220,220)} #HIGH_MOUNTAIN
 
 
 class Generator:
+    """
+    Generate 2D terrain
+    """
     
+    #Can we draw terrain?
+    #Sets false at the end of the screen
     canDraw = True
+    
+    #Start location
     location = locationX, locationY = 0,0
+    
+    #Default terrain type
     tType = 0
     colorList = [[]]
+    
+    #Get a random terrain block to start
     next_terrain = random.choice(terrain_val.values())
     
     def __init__(self):
         self.generate()
     
     def generate(self):
+        """
+        The main generation loop.
+        Handles setting up the next draw, as well as window events
+        """
         while 1:
             if self.canDraw:
-                #self.tType = random.choice(terrain_val.values())
                 print "Next terrain: ", self.next_terrain
                 self.next_terrain = self.draw_terrain(self.next_terrain)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit(); sys.exit();
-            #self.color()
             pygame.display.flip()
+            
+            #Set FPS
             #clock.tick(1)
     
     def next_color(self):
         
-        print "Location Y: ", self.locationY, " Location X: ", self.locationX, " next: ", self.locationX, " , ", self.locationY - terrainSize + 1
-        #if self.locationX >= terrainSize:
-        #    lpix = screen.get_at((self.locationX - terrainSize + 1, self.locationY))
-        #else:
-        #    lpix = -1
-        #print "Pixel at ", self.locationX,", ", self.locationY - terrainSize + 1,": ", screen.get_at((self.locationX, self.locationY - terrainSize + 1)), " and pixel: ", lpix
+        """
+        Get the next terrain color based on either the terrain above and/or to the left of the next tile
+        """
         
+        print "Location Y: ", self.locationY, " Location X: ", self.locationX, " next: ", self.locationX, " , ", self.locationY - terrainSize + 1
+
+        #Get the pixel value of the terrain block above the current location
         if self.locationY > 0 and self.locationY <= height:
             try:
                 top_val = self.terrain_value(terrain_val, screen.get_at((self.locationX, self.locationY - terrainSize + 1)))
             except Exception,e:
                 print e
                 print "tPixels: ", self.locationX, ", ",self.locationY - terrainSize + 1
-                
+        #If there is no block above the current one, set to -1
         else:
             top_val = -1
+            
+        #Get the pixel to the left of the current block
         if self.locationX > 0 and self.locationX <= width:
             try:
                 left_val = self.terrain_value(terrain_val, screen.get_at((self.locationX - terrainSize + 1, self.locationY)))
             except Exception,e:
                 print e
                 print "lPixels: ", self.locationX - terrainSize + 1, ", ",self.locationY
+        #If there is no block to the left, set value to -1
         else:
             left_val = -1
         
+        #Debug
         print "Top: ",top_val, " Left:", left_val
         
         if top_val == -1:
@@ -136,13 +156,17 @@ class Generator:
             return terrain_val[random.choice(ran_list)]
         
     def terrain_value(self, dic, val):
-        """return the key of dictionary dic given the value"""
+        """Return the key of dictionary given the value"""
         try:
             return [k for k, v in dic.iteritems() if v == val][0]
         except IndexError, e:
             print e
     
     def next_location(self):
+        
+        """
+        Set the location to the next block, then get the color for this block
+        """
         
         self.location = self.locationX, self.locationY = self.locationX,self.locationY + terrainSize
         
@@ -153,15 +177,17 @@ class Generator:
         if self.locationX >= width:
             self.canDraw = False
         return self.next_color()
-    #def storeLocation(self,tType):
-    #    self.colorList[self.locationX][self.locationY] = tType
+
     
     def draw_terrain(self, tType):
+        """
+        Draw the next block to the screen
+        """
         pygame.draw.rect(screen, tType, (self.locationX, self.locationY, terrainSize, terrainSize))
-        #self.storeLocation(tType)
         return self.next_location()
         
 if __name__ == "__main__" :
+    #GENERATE!
     Generator()
     
     
